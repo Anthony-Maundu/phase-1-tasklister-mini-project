66cfbddfe51b5dfa367d967cea27ea5f18554fa4
Anthony-Maundu <anthonymaundu1@gmail.com>
2024-07-21 17:59:47 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const { expect } = require("chai");
+const helpers = require("./helpers");
+
+describe("Helpers Module", () => {
+  describe("sampleFunction", () => {
+    it("should return true", () => {
+      const result = helpers.sampleFunction();
+      expect(result).to.be.true;
+    });
+  });
+});
+
 });
